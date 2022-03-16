ab2a29d42e2b99a2b270ea9927cef95db70b1a9e
Rileepher <101723412+Rileepher@users.noreply.github.com>
2022-03-16 22:38:31 +0800

    Update litquiz.sh

--- litquiz.sh
+++ litquiz.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-yum install -y /mnt/usb/downloadrpms/*
-unzip /mnt/usb/webpackets.zip -d /var/www/html
+mkdir -p /var/www/html
+mv /root/clouds-se211-main/webpackets/* /var/www/html/
+yum -y install /root/clouds-se211-main/downloadrpms/*
 systemctl stop firewalld
-setenforce 0
+setenforce 0 
 systemctl restart httpd mariadb
 mysql_secure_installation<<EOF
 
@@ -20,3 +21,4 @@ chmod 777 /var/www/html/nibbleblog/content
 chmod 777 /var/www/html/oscommerce2/catalog/includes/configure.php
 chmod 777 /var/www/html/oscommerce2/catalog/admin/includes/configure.php
 nmcli
+
